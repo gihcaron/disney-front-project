52fda67de24af9942a49a6fdff4e4a448736a7be
Giovanna <giovanna.c.barros@aluno.senai.br>
2025-09-02 16:01:50 -0300

    Feat: adjust responsive of the website

--- src/app/api/page.jsx
+++ src/app/api/page.jsx
@@ -15,7 +15,29 @@ export default function Api() {
     <main className="bg-gray-100">
       <Header />
       <section className={styles.Section}>
-        <div className={styles.SectionBanner}></div>
+        <div className={styles.SectionBanner}>
+            <div className={styles.BannerContent}>
+              <div className={styles.InfoBanner}>
+                <p className={styles.BannerTitle}>Filme Dory • </p>
+                <p className={styles.BannerTitle}>6+ • </p>
+                <p className={styles.BannerTitle}>1h 37m • </p>
+                <p className={styles.BannerTitle}>2020</p>
+              </div>
+
+              <h1 className={styles.BannerText}>Explore a API</h1>
+              <p className={styles.BannerSubText}>A API da Disney é uma API RESTful e GraphQL não oficial que fornece informações sobre personagens, filmes e outros conteúdos da Disney. </p>
+            <div>
+              <div className={styles.PlayButtonContainer}>
+                <button className={styles.PlayButton}>
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8 5V19L19 12L8 5Z" fill="currentColor"/>
+                  </svg>
+                </button>
+                <span className={styles.ExploreText}>Explore mais</span>
+              </div>
+            </div>
+            </div>
+        </div>
       </section>
       <section className={styles.SectionContent}>
         <div className={styles.ApiTitle}>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,71 +15,70 @@ export default function Home() {
       <Header />
       <div className="min-h-screen bg-gray-100 flex items-center justify-center">
 
-        <section className=" h-full max-auto gap-0.5">
+        <section className="h-full max-auto gap-0.5">
 
           <div className="flex flex-col xl:flex-row items-center justify-between pt-4 xl:pb-24">
   
-            {/* Text- Presentation */}
-            <div className="p-6 gap-5 text-center xl:text-left max-w-[800px] flex flex-col space-y-6 order-2 xl:order-none">
-              <span className="font-medium">Desenvolvedora de Sistemas 👩🏻‍💻</span>
-              <h1 className="h1 -4 text-[48px] xl:text-[80px] leading-[1.1] font-semibold">
-                Olá, eu sou a{" "}
-                <span className="text-blue-700">Giovanna Caron</span>
-              </h1>
-              <p className="max-w-[500px] mb-9 text-gray-900">
-                Sou uma <span className="font-medium text-blue-700">desenvolvedora de sistemas</span> do SENAI Valinhos, apaixonada por criar soluções
-                inovadoras. Neste site, você encontrará meu projeto realizado durante o 4o Semestre.
-              </p>
+                  <div className="p-6 gap-5 text-center xl:text-left max-w-[800px] flex flex-col space-y-6 order-2 xl:order-none mb-8 xl:mb-0">
+                    <span className="font-medium hl:mt-9 xl:mb-9">Desenvolvedora de Sistemas 👩🏻‍💻</span>
+                    <h1 className="h1 -4 text-[32px] md:text-[48px] xl:text-[80px] leading-[1.1] font-semibold">
+                    Olá, eu sou a{" "}
+                    <span className="text-blue-700">Giovanna Caron</span>
+                    </h1>
+                    <p className="max-w-[500px] mb-9 text-gray-900 text-sm md:text-base">
+                    Sou uma <span className="font-medium text-blue-700">Desenvolvedora de Sistemas do SENAI Valinhos,</span> apaixonada por criar soluções
+                    inovadoras. Neste site, você encontrará meu projeto realizado durante o 4o Semestre.
+                    </p>
 
-              <p className="max-w-[500px] mb-9 text-gray-900"> <span className="font-medium">Objetivo:</span> Aplicação Web de API da Disney.</p>
-              <p className="max-w-[500px] mb-9 text-gray-900"> <span className="font-medium text-blue-800">Matéria:</span> Front-End II</p>
-              <div className="flex flex-wrap items-start justify-start xl:flex-row xl:items-center xl:text-center gap-8 mt-8 mb-8 xl:mb-0">
-                
-                <div>
-                  <button className="px-8 py-4 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition-colors font-semibold">
-                    Clique aqui
-                  </button>
-                </div>
+                    <p className="max-w-[500px] mb-9 text-gray-900 text-sm md:text-base"> <span className="font-medium text-blue-800">Matéria:</span> Front-End II</p>
+                    <p className="max-w-[500px] mb-9 text-gray-900 text-sm md:text-base">"Liberdade é pouco. O que desejo ainda não tem nome."  <span className="font-medium text-blue-800">- Clarice Lispector</span> </p>
+                    <div className="flex flex-wrap items-start justify-start xl:flex-row xl:items-center xl:text-center gap-8 mt-8 mb-8 xl:mb-0">
+                    
+                    <div>
+                      <button className="px-6 md:px-8 py-3 md:py-4 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition-colors font-semibold text-sm md:text-base">
+                      Clique aqui
+                      </button>
+                    </div>
 
-                <div className="flex flex-wrap gap-4 mt-8 mb-8 xl:mb-0 xl:text-center">
-                  <a
-                    href="https://www.instagram.com/gihcaron?igsh=NTZia2xla2U5ZjNm"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FaInstagram
-                      size={32}
-                      className="text-blue-600 hover:text-blue-800"
-                    />
-                  </a>
-                  <a
-                    href="https://www.linkedin.com/in/giovanna-caron/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FaLinkedin
-                      size={32}
-                      className="text-blue-600 hover:text-blue-800"
-                    />
-                  </a>
-                  <a
-                    href="https://github.com/gihcaron"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FaGithub
-                      size={32}
-                      className="text-blue-600 hover:text-blue-800"
-                    />
-                  </a>
-                </div>
+                    <div className="flex flex-wrap gap-4 mt-8 mb-8 xl:mb-0 xl:text-center">
+                      <a
+                      href="https://www.instagram.com/gihcaron?igsh=NTZia2xla2U5ZjNm"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      >
+                      <FaInstagram
+                        size={20}
+                        className="text-blue-600 hover:text-blue-800"
+                      />
+                      </a>
+                      <a
+                      href="https://www.linkedin.com/in/giovanna-caron/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      >
+                      <FaLinkedin
+                        size={20}
+                        className="text-blue-600 hover:text-blue-800"
+                      />
+                      </a>
+                      <a
+                      href="https://github.com/gihcaron"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      >
+                      <FaGithub
+                        size={20}
+                        className="text-blue-600 hover:text-blue-800"
+                      />
+                      </a>
+                    </div>
 
-              </div>
+                    </div>
 
-            </div>
+                  </div>
 
-            {/* Photo */}
-            <div className="order-1 xl:order-none mb-8 xl:mb-0  ">
+                  {/* Photo */}
+            <div className="order-1 xl:order-none mb-8 xl:mb-0">
               <Photo />
             </div>
           </div>
@@ -90,32 +89,3 @@ export default function Home() {
     </main>
   );
 }
-
-// export default function Home() {
-
-//   return(
-//     <div className="flex items-center justify-center min-h-screen bg-gray-100 border-blue" >
-//       <div className="flex grid-cols-2 items-center rounded-lg shadow-lg h-150 border-2 border-blue-500 max-w-screen-lg px-12 my-4">
-
-//       <div className="p-6 mx-4">
-//         <Image
-//         src="/images/giovanna.jpeg"
-//         alt="Giovanna Caron foto de perfil"
-//         layout="intrinsic"
-//         width={400}
-//         height={600}
-//         className="w-4xl h-150 rounded-2xl" />
-
-//       </div>
-//       <div className=" text-justify-center ">
-//       <h1 className="">Giovanna Caron de Barros</h1>
-//                    <h3  className=""> 2TDS1 - Atividade Avaliativa Front End</h3>
-//                    <p className="">Amados Docentes: Thiago Ferreira e Marcelo Carboni</p>
-//                    <h3  className=""> Conheça a API Estudantes e Projetos:</h3>
-//                    <p className="">Esta API possui 2 entidades: Estudantes e Projetos, onde cada Estudante possui seu projeto relacionado, com o proósito de gerenciar os alunoes de forma eficaz permitindo a visualização de suas atividades e informações pessoais.  </p>
-//                    <p className="">A API permite a consulta e manipulação de dados de forma eficiente.</p>
-//       </div>
-//       </div>
-//     </div>
-//   );
-// }
