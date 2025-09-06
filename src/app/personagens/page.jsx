b0f78c47fbc10d6e9f723595879599735ef1ccae
Giovanna <giovannacaron.tds1.senai@gmail.com>
2025-09-05 22:51:53 -0300

    feat: create banner for personagens page

--- src/app/personagens/page.jsx
+++ src/app/personagens/page.jsx
@@ -1,8 +1,7 @@
-"use client"
-import React, { use } from "react";
+"use client";
 import { useRouter } from "next/navigation";
 import axios from "axios";
-import {Card} from "antd";
+import { Card, Carousel  } from "antd";
 import { Heart } from "lucide-react";
 import { useEffect, useState } from "react";
 
@@ -13,89 +12,106 @@ import Image from "next/image";
 import Header from "../../components/Header";
 
 export default function Personagens() {
+  const [data, setData] = useState(null);
+  const [curtidos, setCurtidos] = useState({});
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
-    const [data, setData] = useState(null);
-    const [curtidos, setCurtidos] = useState({});
-    const router = useRouter();
-    const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const FetchPersonagens = async () => {
+      try {
+        const response = await axios.get("https://api.disneyapi.dev/character");
+        setData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar personagens:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    FetchPersonagens();
+  }, []);
 
-    useEffect(() => {
-        const FetchPersonagens = async () => {
-            try{
-                const response = await axios.get("https://api.disneyapi.dev/character");
-                setData(response.data);
-                console.log(response.data)
+  const handleCurtir = (_id) => {
+    setCurtidos((prev) => ({
+      ...prev,
+      [_id]: !prev[_id],
+    }));
+  };
 
-            }
-            catch(error){
-                console.error("Erro ao buscar personagens:", error);
-            }
-            finally {
-                setLoading(false);
-            }
-            
-        }
-        FetchPersonagens();
-    }, []);
-
-    const handleCurtir = (_id) => {
-      setCurtidos((prev) => ({
-        ...prev,
-        [_id]: !prev[_id]
-      }))
-    }
-
+  const contentStyle = {
+  height: '340px',
+  color: '#fff',
+  lineHeight: '200px',
+  textAlign: 'center',
+};
 
   return (
     <main className={styles.main}>
-        <Header />
+      <Header />
       <section className={styles.banner}>
-        <h1 className={styles.title}>Conheça os Personagens</h1>
+      {/* Banner aqui */}
+         <Carousel autoplay>
+    <div className={styles.carouselSlide1}>
+      <h3 style={contentStyle}></h3>
+    </div>
+    <div className={styles.carouselSlide2}>
+      <h3 style={contentStyle}>2</h3>
+    </div>
+    <div className={styles.carouselSlide3}>
+      <h3 style={contentStyle}>3</h3>
+    </div>
+    <div className={styles.carouselSlide4}>
+      <h3 style={contentStyle}>4</h3>
+    </div>
+  </Carousel>
       </section>
-    <section className={styles.personagens}>
-       <div className={styles.cardsContainer}>
-  {loading && <p>Carregando...</p>}
-        {data?.data?.map((character) => (
-          <Card
-            key={character._id}
-            className={styles.card}
-            hoverable
-            cover={
+      <section className={styles.personagens}>
+        <div className={styles.cardsContainer}>
+          {loading && <p>Carregando...</p>}
+          {data?.data?.map((character) => (
+            <Card
+              key={character._id}
+              className={styles.card}
+              hoverable
+              cover={
                 <Image
-                    alt={character.name}
-                    className={styles.image}
-                    src={character.imageUrl || "/images/userPadrao.jpg"}
-                    width={150}
-                    height={150}
+                  alt={character.name}
+                  className={styles.image}
+                  src={character.imageUrl || "/images/userPadrao.jpg"}
+                  width={150}
+                  height={150}
                 />
-            }
-          >
-            <Card.Meta
-            title={character.name}
-            description={character.films?.join(", ") || "Sem filmes"}
-
-            />
-        <div className={styles.detailsContainer}>
-        <div className={styles.details}>
-            <p onClick={() => router.push(`/character/${character._id}`)} className={styles.MoreDetails}>Ver Detalhes</p>
+              }
+            >
+              <Card.Meta
+                title={character.name}
+                description={character.films?.join(", ") || "Sem filmes"}
+              />
+              <div className={styles.detailsContainer}>
+                <div className={styles.details}>
+                  <p
+                    onClick={() => router.push(`/character/${character._id}`)}
+                    className={styles.MoreDetails}
+                  >
+                    Ver Detalhes
+                  </p>
+                </div>
+                <div className={styles.heart}>
+                  <Heart
+                    size={28}
+                    color={curtidos[character._id] ? "#1d4ed8" : "black"}
+                    fill={curtidos[character._id] ? "#1d4ed8" : "none"}
+                    style={{ cursor: "pointer" }}
+                    aria-label="Curtir"
+                    onClick={() => handleCurtir(character._id)}
+                  />
+                </div>
+              </div>
+            </Card>
+          ))}
         </div>
-         <div className={styles.heart}>
-            <Heart
-              size={28}
-              color={curtidos[character._id] ? "#1d4ed8" : "black"}
-              fill={curtidos[character._id] ? "#1d4ed8" : "none"}
-              style={{ cursor: "pointer" }}
-              aria-label="Curtir"
-              onClick={() => handleCurtir(character._id)}
-            />
-         </div>
-        </div>
-          </Card>
-          
-        ))}
-
-       </div>
-    </section>
+      </section>
     </main>
-  )
+  );
 }
